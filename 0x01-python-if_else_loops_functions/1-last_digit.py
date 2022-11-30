4807ea21199d73f32be622cdf1317a324c6043e7
Bonkesam <bonkesam9@gmail.com>
2022-11-30 13:12:06 -0800

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if mod > 5:
     print(f"Last digit of {number} is {mod} and is greater than 5")
 elif mod == 0:
     print(f"Last digit of {number} is {mod} and is 0")
-else:
+elif mod < 6 and mod is not 0:
     print(f"Last digit of {number} is {mod} and is less than 6 and not 0")

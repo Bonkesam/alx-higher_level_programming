2fc40c64ad5f08b66df9929d3893041bbf5e18ea
Bonkesam <bonkesam9@gmail.com>
2022-11-30 16:20:10 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number >= 0:
-        mod  = number % 10
-    else:
-        mod = (number % 10) -10
-
+    mod = abs(number) % 10
     return (mod)

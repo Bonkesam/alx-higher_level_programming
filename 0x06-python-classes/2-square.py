ce35925aaf60524573afa03a2a19510f0cf8e4e7
Bonkesam <bonkesam9@gmail.com>
2023-01-30 02:50:30 -0800

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+class Square:
+    """Represent a Square"""
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args:
+          size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be intager")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
